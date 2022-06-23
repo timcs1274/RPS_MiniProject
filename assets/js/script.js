f9c6d71efb6746fc84826736dae0c1e42f98dca8
Brice Veyna <bricev99@gmail.com>
2022-06-23 12:22:20 -0700

    Update if statement, variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,37 @@ var S = 2;
 //0=R 1=P 2=S
 var computerInput = math.random() * 2;
 
-if (playerInput == computerInput){
-    alert("You've tied!")
-};
+var win = 0;
+var lose = 0;
+var tie = 0;
 
-if (playerInput = R && computerInput === 1){
-    alert("Paper beats rock, you lose!")
+if (playerInput === "R" && computerInput === 0){
+    tie ++;
+    alert("You've tied!");
+} else if (playerInput === "R" && computerInput === 1){
+    lose ++;
+    alert("Paper beats rock, you lose!");
+} else if (playerInput === "R" && computerInput === 2){
+    win ++;
+    alert("Rock beats scissors, you win!");
+} else if (playerInput === "P" && computerInput === 0){
+    win ++;
+    alert("Paper beats rock, you win!");
+} else if (playerInput === "P" && computerInput === 1){
+    tie ++;
+    alert("You've tied!");
+} else if (playerInput === "P" && computerInput === 2){
+    lose ++;
+    alert("Scissors beats paper, you lose!");
+} else if (playerInput === "S" && computerInput === 0){
+    lose ++;
+    alert("Rock beats scissors, you lose!");
+} else if (playerInput === "S" && computerInput === 1){
+    win ++;
+    alert("Scissors beats paper, you win!");
+} else if (playerInput === "S" && computerInput === 2){
+    tie ++;
+    alert("You've tied!");
+} else {
+    alert("Invalid input");
 }
-
-
-
-
-
