8d6205847ea160f72eb66ed8bcc8a9e98b5ecdca
timcs <timcs1274@gmail.com>
2022-06-23 14:53:43 -0400

    base code

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+alert("R = Rock P = Paper S = Scissors");
+var playerInput = prompt("Input your choice here: ");
+var R = 0;
+var P = 1;
+var S = 2;
+
+//0=R 1=P 2=S
+var computerInput = math.random() * 2;
+
+if (playerInput == computerInput){
+    alert("You've tied!")
+};
+
+if (playerInput = R && computerInput === 1){
+    alert("Paper beats rock, you lose!")
+}
+
+
+
+
+
